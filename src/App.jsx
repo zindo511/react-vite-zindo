4f93efe0430780ae9cccc3035afcdb0bddce2fa2
Quang Huy <huyquang2k5@gmail.com>
2025-08-09 15:38:14 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+// () => { }
+const App = () => {
   const [count, setCount] = useState(0)
 
+  // function myFunction() {
+  //   console.log(">>run my function")
+  // }
+
+  const myFunction = () => {
+    console.log(">>run my arrow function")
+  }
+  myFunction()
   return (
     <>
       <div>
