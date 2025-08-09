a941a4a67e11f993d3ea1c4792c58e1f81026c93
Quang Huy <huyquang2k5@gmail.com>
2025-08-09 16:59:25 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,9 +4,20 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const huy = "dz1" //string
+    // const huy = 25 //number
+    // const huy = true //boolean
+    // const huy = undefined
+    // const huy = null
+    const huy = [1, 2, 3]
+    // const huy = {
+    //     name: "dz",
+    //     age: 25
+    // }
     return (
       <> 
-        <div className="">huy & dz update</div> 
+        <div> {JSON.stringify(huy)} & dz update</div> 
+        <div>{console.log("HUY")}</div>
         <div className="child"
         style={
             {borderRadius: "10px"}
