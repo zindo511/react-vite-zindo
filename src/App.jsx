a644c5481f298dd6e529068fd08f89f3adcf831e
Quang Huy <huyquang2k5@gmail.com>
2025-08-09 15:51:14 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// () => { }
+//() => { }
+//component = HTML + CSS + JS
+const MyComponent = () => {
+  return (
+    <div>huy & dz update</div>
+  );
+}
 const App = () => {
   const [count, setCount] = useState(0)
 
-  // function myFunction() {
-  //   console.log(">>run my function")
-  // }
 
-  const myFunction = () => {
-    console.log(">>run my arrow function")
-  }
-  myFunction()
   return (
     <>
       <div>
@@ -26,6 +25,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello Word & huy</h1>
+      <MyComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
