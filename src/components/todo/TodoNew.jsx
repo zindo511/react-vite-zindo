2581f96c5814a3137d53cb2d86cf82a0263af12b
Quang Huy <huyquang2k5@gmail.com>
2025-08-15 16:36:18 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,26 @@
 const TodoNew = (props) => {
-  console.log(props)
-  const {addNewTodo} = props
-  addNewTodo("huy")
+  console.log(props);
+  const { addNewTodo } = props;
+  // addNewTodo("huy")
+
+  const handleClick = () => {
+    alert("click me");
+  };
+
+  const handleOnChange = (event) => {
+    console.log("handleOnChange", event.target.value);
+  };
   return (
     <div className="todo-new">
-      <input type="text" placeholder="" />
-      <button className="btn">Add</button>
+      <input type="text" placeholder="" 
+        onChange={(event) => handleOnChange(event.target.value)} />
+      <button
+        style={{ cursor: "pointer" }}
+        className="btn"
+        onClick={handleClick}
+      >
+        Add
+      </button>
     </div>
   );
 };
